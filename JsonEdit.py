103d620b58ddc75f9045e238e43e00da0ee89af7
Colum Charlton <columcharlton@sidero.ie>
2020-01-27 17:43:29 +0000

    Adding build

---
+++ JsonEdit.py
@@ -0,0 +1,193 @@
+import time
+import re
+import json
+from subprocess import check_output
+
+
+class JsonEditer:
+
+    def retrieveKey(text):
+        """Remove all divs from a string and return"""
+        try:
+            pattern = re.compile('\w.*\n')
+            f = ''.join(re.findall(pattern, text))  # Joins strings otherwise error is produced
+            return f
+        except Exception as e:
+            print(e)
+
+    def retrieveValueAfterNewLine(text):
+        """Remove all divs from a string and return"""
+        try:
+            pattern = re.compile('\n\w.*')
+            f = ''.join(re.findall(pattern, text))  # Joins strings otherwise error is produced
+            return f
+        except Exception as e:
+            print(e)
+
+    def removeAfterNewLine(text):
+        """Remove all divs from a string and return"""
+        try:
+            pattern = re.compile('\n\w.*')
+            f = ''.join(re.sub(pattern, '', text))  # Joins strings otherwise error is produced
+            return f
+        except Exception as e:
+            print(e)
+
+    def removeBeforeNewLine(text):
+        """Remove html tags from a string"""
+        try:
+            pattern = '\w.*\n'
+            clean = re.compile(pattern)  # Match all occurences of characters inside the bracets
+            f = ''.join(re.sub(clean, '', text))  # Joins strings otherwise error is produced
+            return f
+        except Exception as e:
+            print(e)
+
+    def remove_whiteSpace(text):
+        try:
+            pattern = '\s'  # match all white space
+            t = re.split(pattern, text)
+            # t = re.findall(pattern, text)
+            whiteSpace = ' '.join(t)
+            return whiteSpace
+        except Exception as e:
+            print(e)
+
+
+    #basic one column
+    def dictMethod(output, string):
+        k = JsonEditer.removeAfterNewLine(output)
+        key = JsonEditer.remove_whiteSpace(k)
+        # print(key)
+        v = JsonEditer.retrieveValueAfterNewLine(output)
+        value = JsonEditer.remove_whiteSpace(v)
+
+        return {string: [key, value]}
+
+
+    # two columns
+    def twoColumns(output, string):
+        # everything before and after the tab
+        '^.*\t([^\t]+)$'
+
+        # everything before the tab
+        # key = '\w.*\t'
+        key = '^.*\t'
+        # everything after the tab
+        value = '([^\:]+)$'
+        v = '\t.*\n'
+
+        # split output on new line
+        m = re.split('\n', output)
+
+        myD = {}
+        for i in range(len(m)):
+            keyTuple = ''.join(re.findall('.*\t', m[i]))
+            cleankey = ''.join(re.sub('\t', '', keyTuple))
+            valueTuple = ''.join(re.findall('\:.*', m[i]))
+            cleanValue = ''.join(re.sub(':', '', valueTuple))
+            myD.update({cleankey: cleanValue})
+        i += 1
+        return {string: myD}
+
+
+
+    def beforeForwardSlashAndafterBackSlash(output, string):
+        m = re.split('\n', output)
+        myDict = {}
+
+        i=0
+        for i in range(len(m)):
+            keyTuple = ''.join(re.findall('(^\S*/)', m[i]))
+            valueTuple = ''.join(re.findall('\/.*', m[i]))
+            myDict.update({keyTuple: valueTuple})
+        i += 1
+        return {string: myDict}
+
+    def beforeforwardslashAndaftercolon(output, help):
+
+        myD = {}
+        m = re.split('\n', output)
+        # print(m[0])
+
+        i = 0
+        for i in range(len(m)):
+            # print(m[i])
+            keyTuple = ''.join(re.findall('.*\:', m[i]))
+            cleankey = ''.join(re.sub(':', '', keyTuple))
+            valueTuple = ''.join(re.findall('\:.*', m[i]))
+            cleanValue = ''.join(re.sub(':', '', valueTuple))
+            if valueTuple == '' and keyTuple == '':
+                if m[i] != '':
+                    cleankey = ''
+                    cleanValue = m[i]
+                else:
+                    break
+
+            # string = re.sub(" ", '', valueTuple)
+            myD.update({cleankey: cleanValue})
+        i += 1
+
+        return {help: myD}
+
+    def whiteSpaceAndBackslash(output, string):
+        myD = {}
+        m = re.split('\n', output)
+        i = 0
+        for i in range(len(m)):
+            keyTuple = ''.join(re.findall('(^\S*)', m[i]))
+            valueTuple = ''.join(re.findall('\/.*', m[i]))
+            myD.update({keyTuple: valueTuple})
+        i += 1
+
+        return {string: myD}
+
+
+    def beforeEqualsAndAfterEquals(output, string):
+        m = re.split('\n', output)
+        myDict = {}
+        i = 0
+        for i in range(len(m)):
+            keyTuple = ''.join(re.findall('(^\S*=)', m[i]))
+            cleanKey = ''.join(re.sub('=', '', keyTuple))
+            valueTuple = ''.join(re.findall('\=.*', m[i]))
+            cleanValue = ''.join(re.sub('=', '', valueTuple))
+
+            myDict.update({cleanKey: cleanValue})
+        i += 1
+        return {string: myDict}
+
+
+    def beforeWhitespacesAndAfterWhitespace(output, string):
+        m = re.split('\n', output)
+        myDict = {}
+        i = 1
+        for i in range(len(m)):
+            keyTuple = ''.join(re.findall('(^\S*)', m[i]))
+            valueTuple = ''.join(re.sub('(^\S*)', '', m[i]))
+            cleanValue = ''.join(re.sub('\s', '', valueTuple))
+            myDict.update({keyTuple: cleanValue})
+        i += 1
+
+        return {string: myDict}
+
+
+
+
+    def saveAsJson(words, save):
+        try:
+            with open(save, "w") as text_file:
+                print(f'{words}', file=text_file)
+        except Exception as e:
+            print(e)
+
+
+    def jsonCreater(myDict, os):
+
+        # import json
+        # myDict = {}
+        jsonData = json.dumps(myDict, indent=4)
+        print(jsonData)
+        JsonEditer.saveAsJson(jsonData, os +'.json')
+
+
---
+++ test.py
@@ -0,0 +1,140 @@
+import unittest
+from sys import platform
+import OsCheck
+import oSCommands
+from oSCommands import oS
+# from jsonFormattedWindowsCommands import winSys
+from JsonEdit import JsonEditer
+from LinuxCommands import linux
+
+class TestOsCheck(unittest.TestCase):
+
+    def test_os(self):
+        os = OsCheck.systemCheck()
+        a = platform
+        self.assertEqual(os.platfrom(), platform)
+
+
+class TestOsCommands(unittest.TestCase):
+    from oSCommands import oS
+
+
+    def test_command2(self):
+
+        self.assertIsInstance(oS.command2("system"), str)
+        # self.assertEqual(oS.command2(), )
+
+    def test_commandWin(self):
+
+        self.assertIsInstance(oS.command2("system"), str)
+        # self.assertEqual(oS.command2(), )
+
+    def test_commandLinux(self):
+
+        self.assertIsInstance(oS.command2("system"), str)
+        # self.assertEqual(oS.command2(), )
+
+
+
+class TestFormattedWindowsCommands(unittest.TestCase):
+
+
+    if platform == "win32":
+        from WindowsCommands import winSys
+
+        def test_Hardware(self):
+
+            from WindowsCommands import winSys
+            self.assertIsInstance(winSys.Hardware(self), dict)
+
+        def test_Software(self):
+            from WindowsCommands import winSys
+            self.assertIsInstance(winSys.Software(self), dict)
+
+        def test_Firmware(self):
+            from WindowsCommands import winSys
+            self.assertIsInstance(winSys.Driver_Firmware(self), dict)
+
+        def test_Services(self):
+            from WindowsCommands import winSys
+            self.assertIsInstance(winSys.Services(self), dict)
+
+        def test_Hostname_Startup(self):
+            from WindowsCommands import winSys
+            self.assertIsInstance(winSys.Hostname_Startup(self), dict)
+
+        def test_MountedDrivers_MappedNetworks(self):
+            from WindowsCommands import winSys
+            self.assertIsInstance(winSys.MountedDrivers_MappedNetworks(self), dict)
+
+        def test_Env(self):
+            from WindowsCommands import winSys
+            self.assertIsInstance(winSys.Env(self), dict)
+
+    else:
+        print(platform + "platform not compatable with current tests " )
+
+
+
+
+
+class TestJsonEdit(unittest.TestCase):
+    # from jsonFormattedWindowsCommands import winSys
+
+
+    def test_JsonEdit(self):
+        self.assertIsInstance(JsonEditer.dictMethod(self, "hello"), dict)
+
+    def test_twoColumns(self):
+        self.assertIsInstance(JsonEditer.twoColumns( "hello", "hello"), dict)
+
+    def test_beforeEqualsAndAfterEquals(self):
+        self.assertIsInstance(JsonEditer.beforeEqualsAndAfterEquals("hello", "hello"), dict)
+
+    def test_beforeForwardSlashAndafterBackSlash(self):
+        self.assertIsInstance(JsonEditer.beforeForwardSlashAndafterBackSlash("hello", "hello"), dict)
+
+    def test_beforeForwardSlashAndafterBackSlash(self):
+        self.assertIsInstance(JsonEditer.beforeforwardslashAndaftercolon("hello", "hello"), dict)
+
+    def beforeWhitespacesAndAfterWhitespace(self):
+        self.assertIsInstance(JsonEditer.beforeWhitespacesAndAfterWhitespace("hello", "hello"), dict)
+
+
+class TestJsonFormattedLinuxCommands(unittest.TestCase):
+    # from jsonFormattedLinuxCommands import linux
+    if platform == "linux" or platform == "linux2":
+
+        def test_Hardware(self):
+            self.assertIsInstance(linux.Hardware(), dict)
+
+        def test_Software(self):
+            self.assertIsInstance(linux.Software(), dict)
+
+        def test_Firmware(self):
+            self.assertIsInstance(linux.Driver_Firmware(), dict)
+
+        def test_Services(self):
+            self.assertIsInstance(linux.Services(), dict)
+
+        def test_Hostname_Startup(self):
+            self.assertIsInstance(linux.Hostname_Startup(), dict)
+
+        def test_MountedDrivers_MappedNetworks(self):
+            self.assertIsInstance(linux.MountedDrivers_MappedNetworks(), dict)
+
+        def test_Env(self):
+            self.assertIsInstance(linux.Env(), dict)
+
+
+    else:
+        print(platform + "platform not compatable with current tests " )
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    unittest.main()
